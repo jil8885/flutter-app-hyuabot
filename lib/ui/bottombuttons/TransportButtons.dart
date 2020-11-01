ba96a4eabe18f5dd59e5edb6f4036cc41e5bca06
Jeongin <jil8885@hanyang.ac.kr>
2020-11-01 17:14:35 +0900

    Prepare to make bottom sheet

--- lib/ui/bottombuttons/TransportButtons.dart
+++ lib/ui/bottombuttons/TransportButtons.dart
@@ -1,5 +1,6 @@
 import 'package:chatbot/main.dart';
 import 'package:chatbot/config/common.dart';
+import 'package:chatbot/ui/bottomsheets/TransportSheets.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
@@ -37,7 +38,7 @@ class TransportMenuButtons extends StatelessWidget{
             elevation: 6,
             onPressed: (){
               subButtonController.updateSubButtonIndex(index);
-              showMaterialModalBottomSheet(context: context, builder: null);
+              showMaterialModalBottomSheet(context: context, builder: (context, scrollController) => TransportSheets());
             },
           ),
         );
---
+++ lib/ui/bottomsheets/TransportSheets.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TransportSheets extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
