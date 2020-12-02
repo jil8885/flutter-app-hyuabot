dedf25ed0998365fc3aee65c97ff55404aba8160
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-12-02 13:04:26 +0900

    Complete Bus Page

---
+++ lib/Page/BusPage.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app_hyuabot_v2/Config/AdManager.dart';
+import 'package:flutter_app_hyuabot_v2/Config/GlobalVars.dart';
+import 'package:flutter_app_hyuabot_v2/UI/CustomPaint/BusCardPaint.dart';
+import 'package:flutter_native_admob/flutter_native_admob.dart';
+import 'package:flutter_native_admob/native_admob_options.dart';
+import 'package:get/get.dart';
+
+class BusPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BusPageState();
+
+}
+class _BusPageState extends State<BusPage> with SingleTickerProviderStateMixin{
+  Timer _busTimer;
+
+
+  Widget _busCard(double width, String busStop, String terminalStop, String lineName, Color lineColor, Map<String, dynamic> data, bool timeTableOffered){
+    return Card(
+      color: Colors.white,
+      elevation: 3,
+      child: Container(
+        width: width - 50,
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 5),
+              child: Text(busStop, style: TextStyle(fontSize: 16, fontFamily: "Godo", color: Colors.black),),
+            ),
+            Text("$terminalStop 방면", style: TextStyle(fontSize: 12, fontFamily: "Godo", color: Colors.grey),),
+            Divider(color: Colors.grey),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(child: Text(lineName, style: TextStyle(color: lineColor, fontSize: 32, fontFamily: "Godo"),), padding: const EdgeInsets.only(top: 5, bottom: 10), height: 50, width: width / 2.5,),
+                CustomPaint(painter: BusCardPaint(data, lineColor, context, timeTableOffered),)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    _busTimer = Timer.periodic(Duration(minutes: 1), (timer) {
+      busController.fetch();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: StreamBuilder<Map<String, dynamic>>(
+          stream: busController.allBusInfo,
+          builder: (context, snapshot) {
+            if(snapshot.hasError){
+              return Center(child: Text("버스 정보를 불러오는데 실패했습니다.", style: Theme.of(context).textTheme.bodyText1,),);
+            } else if(!snapshot.hasData){
+              return Center(child: CircularProgressIndicator(),);
+            }
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(icon: Icon(Icons.arrow_back_rounded, color: Theme.of(context).textTheme.bodyText1.color,), onPressed: (){Get.back();}, padding: EdgeInsets.only(left: 20),)
+                    ],
+                  ),
+                  _busCard(_width, "한양대게스트하우스", "상록수역", "10-1", Color(0xff009e96), snapshot.data['10-1'], true),
+                  _busCard(_width, "한양대게스트하우스", "강남역", "3102", Color(0xffe60012), snapshot.data['3102'], true),
+                  _busCard(_width, "한양대정문", "수원역", "707-1", Color(0xff0068b7), snapshot.data['707-1'], false),
+                  Expanded(
+                    child: Center(child: Text("버스 시간표 등 기타 정보는\n각 노선을 터치해보세요!", style: TextStyle(color: Theme.of(context).textTheme.bodyText1.color), textAlign: TextAlign.center,)),
+                  ),
+                  Container(
+                    height: 90,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    child: NativeAdmob(
+                      adUnitID: AdManager.bannerAdUnitId,
+                      numberAds: 1,
+                      controller: adController,
+                      type: NativeAdmobType.banner,
+                      error: Center(child: Text("광고 불러오기 실패", style: TextStyle(color: Theme.of(context).textTheme.bodyText1.color, fontSize: 14), textAlign: TextAlign.center,)),
+                      options: NativeAdmobOptions(
+                        adLabelTextStyle: NativeTextStyle(color: Theme.of(context).textTheme.bodyText1.color,),
+                        bodyTextStyle: NativeTextStyle(color: Theme.of(context).textTheme.bodyText1.color),
+                        headlineTextStyle: NativeTextStyle(color: Theme.of(context).textTheme.bodyText1.color),
+                        advertiserTextStyle: NativeTextStyle(color: Theme.of(context).textTheme.bodyText1.color),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _busTimer.cancel();
+    super.dispose();
+  }
+}
---
+++ lib/UI/CustomPaint/BusCardPaint.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_hyuabot_v2/Model/Bus.dart';
+
+class BusCardPaint extends CustomPainter{
+  final Map<String, dynamic> data;
+  final Color lineColor;
+  final BuildContext context;
+  bool timeTableOffered;
+
+  BusCardPaint(this.data, this.lineColor, this.context, this.timeTableOffered);
+
+  void drawRemainedTime(Canvas canvas, Offset offset, String text, BuildContext context) {
+    TextSpan sp = TextSpan(style: TextStyle(color: Colors.black, fontSize: 16, fontFamily: 'Godo'), text: text);
+    TextPainter tp = TextPainter(text: sp, textDirection: TextDirection.ltr);
+    tp.layout();
+    Offset location = Offset(offset.dx, offset.dy - tp.height * .5);
+    tp.paint(canvas, location);
+  }
+
+  void drawInfo(Canvas canvas, Offset offset, int numOfStop, BuildContext context) {
+    TextSpan sp = TextSpan(style: TextStyle(color: Colors.grey, fontSize: 12, fontFamily: 'Godo'), text: '$numOfStop번째 전');
+    TextPainter tp = TextPainter(text: sp, textDirection: TextDirection.ltr);
+    tp.layout();
+    Offset location = Offset(offset.dx, offset.dy - tp.height * .5);
+    tp.paint(canvas, location);
+  }
+
+  void drawSeat(Canvas canvas, Offset offset, int seats, Color lineColor, BuildContext context) {
+    TextSpan sp = TextSpan(style: TextStyle(color: lineColor, fontSize: 12, fontFamily: 'Godo'), text: '$seats석');
+    TextPainter tp = TextPainter(text: sp, textDirection: TextDirection.ltr);
+    tp.layout();
+    Offset location = Offset(offset.dx, offset.dy - tp.height * .5);
+    tp.paint(canvas, location);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Paint for line
+    final _line = Paint()
+      ..color = Colors.grey
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2.0;
+
+    final _boxColor = Paint()
+      ..color = Colors.grey.withOpacity(0.6)
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2.0;
+
+    // Paint for White circle
+    final _white = Paint()
+      ..color = Colors.white
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2.0;
+
+    // Paint for line Circle
+    final _colorPaint = Paint()
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2.0;
+
+    double _dashWidth = 3;
+    double _dashSpace = 2;
+    double _start = 10;
+    final space = (_dashSpace + _dashWidth);
+
+    while (_start < 35) {
+      canvas.drawLine(Offset(10, _start), Offset(10, _start + _dashSpace), _line);
+      _start += space;
+    }
+
+    canvas.drawCircle(Offset(10, 10), 5.0, _colorPaint);
+    canvas.drawCircle(Offset(10, 35), 5.0, _line);
+    canvas.drawCircle(Offset(10, 35), 3.0, _white);
+
+    List<BusInfoRealtime> realtimeList = data['realtime'];
+    List<BusInfoTimetable> timetableList = data['timetable'];
+
+      if(realtimeList.length >= 2){
+        drawRemainedTime(canvas, Offset(25, 10), '${realtimeList.elementAt(0).time}분', context);
+        drawRemainedTime(canvas, Offset(25, 35), '${realtimeList.elementAt(1).time}분', context);
+      } else if(realtimeList.length == 1){
+        drawRemainedTime(canvas, Offset(25, 10), '${realtimeList.elementAt(0).time}분', context);
+        if(realtimeList.elementAt(0).seats != -1){
+          canvas.drawRRect(RRect.fromRectAndRadius(Rect.fromLTWH(60, 2, 77.5, 16), Radius.circular(1.5)), _boxColor);
+          canvas.drawRect(Rect.fromLTWH(61, 3, 75.5, 14), _white);
+          drawInfo(canvas, Offset(62.5, 10), realtimeList.elementAt(0).location, context);
+          drawSeat(canvas, Offset(110, 10), realtimeList.elementAt(0).seats, lineColor, context);
+        } else {
+          canvas.drawRRect(RRect.fromRectAndRadius(Rect.fromLTWH(60, 2, 45, 16), Radius.circular(1.5)), _line);
+          canvas.drawRect(Rect.fromLTWH(61, 3, 43, 14), _white);
+          drawInfo(canvas, Offset(62.5, 10), realtimeList.elementAt(0).location, context);
+        }
+        if(timetableList.isNotEmpty){
+          drawRemainedTime(canvas, Offset(25, 35), '${timetableList.elementAt(0).time} 출발', context);
+        } else {
+          if(timeTableOffered) {
+            drawRemainedTime(canvas, Offset(25, 35), '막차입니다.', context);
+          } else {
+            drawRemainedTime(canvas, Offset(25, 35), '시간표 미제공', context);
+          }
+        }
+      } else if (!timeTableOffered){
+        drawRemainedTime(canvas, Offset(25, 10), '시간표 미제공', context);
+        drawRemainedTime(canvas, Offset(25, 35), '시간표 미제공', context);
+      } else if(timetableList.length >= 2) {
+        drawRemainedTime(canvas, Offset(25, 10), '${timetableList.elementAt(0).time} 출발', context);
+        drawRemainedTime(canvas, Offset(25, 35), '${timetableList.elementAt(1).time} 출발', context);
+      } else if (timetableList.length == 1){
+        drawRemainedTime(canvas, Offset(25, 10), '${timetableList.elementAt(0).time} 출발', context);
+        drawRemainedTime(canvas, Offset(25, 35), '막차입니다.', context);
+      } else {
+        drawRemainedTime(canvas, Offset(25, 10), '운행 종료', context);
+      }
+  }
+
+  @override
+  bool shouldRepaint(covariant BusCardPaint oldDelegate) {
+    if(oldDelegate.data != this.data){
+      return true;
+    } else{
+      return false;
+    }
+  }
+}
