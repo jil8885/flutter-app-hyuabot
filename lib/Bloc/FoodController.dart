dc6e4c392b75138008e2ca696c2cb972b4bdd077
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-12-09 14:16:18 +0900

    Fix smal changes

--- lib/Bloc/FoodController.dart
+++ lib/Bloc/FoodController.dart
@@ -18,7 +18,7 @@ class FetchFoodInfoController{
     Map<String, Map<String, List<FoodMenu>>> allMenus = {};
 
     final url = Uri.encodeFull(conf.getAPIServer() + "/app/food");
-    http.Response response = await http.post(url, body: jsonEncode({'language': prefManager.getString("localeCode").split("_")[0]}));
+    http.Response response = await http.post(url, body: jsonEncode({'language': (prefManager.getString("localeCode") ?? 'ko').split("_")[0]}));
     Map<String, dynamic> responseJson = jsonDecode(utf8.decode(response.bodyBytes));
     for(String name in responseJson.keys){
       if(name.contains("erica")){
