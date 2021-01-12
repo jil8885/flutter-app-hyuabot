6baaee65c0006a442464641d2f55c7371aba36cd
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-01-12 16:17:53 +0900

    Add Location manager

--- android/app/src/main/kotlin/app/kobuggi/hyuabot/home_widgets/ShuttleHomeWidgetConfig.kt
+++ android/app/src/main/kotlin/app/kobuggi/hyuabot/home_widgets/ShuttleHomeWidgetConfig.kt
@@ -1,15 +1,25 @@
 package app.kobuggi.hyuabot.home_widgets
 
+import android.Manifest
 import android.app.Activity
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.RadioGroup
 import android.widget.RemoteViews
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import app.kobuggi.hyuabot.AppLocationListener
 import app.kobuggi.hyuabot.MainActivity
 import app.kobuggi.hyuabot.R
 
@@ -20,12 +30,10 @@ class ShuttleHomeWidgetConfig : Activity (){
 
     private var appWidgetID : Int = AppWidgetManager.INVALID_APPWIDGET_ID;
     private lateinit var radioGroup : RadioGroup
-    private lateinit var widgetTransparent : SeekBar
     private var selectedValue : String = "auto"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shuttle_widget_config)
-
         val configIntent : Intent = intent
         val extras : Bundle? = configIntent.extras
         if(extras != null){
@@ -41,7 +49,22 @@ class ShuttleHomeWidgetConfig : Activity (){
         }
 
         radioGroup = findViewById(R.id.shuttleRadioGroup)
-//        widgetTransparent = findViewById(R.id.shuttleWidgetTransparent)
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, this.resources.getString(R.string.required_location_permission), Toast.LENGTH_LONG).show();
+            } else{
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION), 100)
+                } else {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
+                }
+            }
+        } else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 100)
+            }
+        }
     }
 
     fun confirmConfiguration(view : View){
@@ -68,11 +91,20 @@ class ShuttleHomeWidgetConfig : Activity (){
         val stopList = listOf("dorm", "outSchool", "station", "terminal")
         val latitudeList = listOf(37.293504675319404, 37.29875067621797, 37.3078222739517, 37.31925490540365)
         val longitudeList = listOf(126.83652294056917, 126.83795526758001, 126.85385203121884, 126.84558571149732)
-        var distance = 9999999
+        var distance : Double
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return
+        }
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationListener = AppLocationListener(this)
+
+        val location = locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0 as Float, locationListener)
         if(selectedValue == "auto"){
             for (i in 0..3){
 
             }
+            Toast.makeText(this, locationListener.toString(), Toast.LENGTH_LONG)
             stopName = "outSchool"
         } else {
             stopName = this.getString(this.resources.getIdentifier(selectedValue, "string", this.packageName))
